5943294ef83f4614e982dea639c52d4c24623c44
iiwalter <iwalterhcf@outlook.com>
2019-12-05 17:34:50 -0300

    Registrar as models no arquivo admin.py

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from website.models import Cadastro
+from website.models import Alagou
 # Register your models here.
+
+admin.site.register(Cadastro)
+admin.site.register(Alagou)
