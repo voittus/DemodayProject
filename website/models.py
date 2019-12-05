6055b81d199f2ea2600c29109f000bd142ab7152
iiwalter <iwalterhcf@outlook.com>
2019-12-05 17:36:10 -0300

    Adicionar os dados as configurações dos campos das forms no models.py

--- website/models.py
+++ website/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from django import forms
 # Create your models here.
+class Cadastro(models.Model):
+    nome = models.CharField(max_length=140)
+    email = models.EmailField()
+    senha = models.CharField(max_length=140)
+    def __str__(self):
+        return self.nome
+
+class Alagou(models.Model):
+    bairro = models.CharField(max_length=200)
+    situacao = models.CharField(max_length=300)
+    trafego = models.CharField(max_length=150)
+    def __str__(self):
+        return self.bairro
