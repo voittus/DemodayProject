22a3a52bbe40b38583d9236101a9eca4b3e25925
Walter Henrique Costa Ferreira <walter.ferreira@rcky.com.br>
2019-12-05 17:32:14 -0300

    Adicionar as urls patterns no arquivo urls.py

--- Voittus/urls.py
+++ Voittus/urls.py
@@ -19,5 +19,7 @@ from website import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('' , views.home)
+    path('', views.home),
+    path('alagou/', views.alagou),
+    path('cadastro/', views.cadastro)
 ]
