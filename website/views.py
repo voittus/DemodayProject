6257c55bf1df6007d993920f2947cffa0c1c123f
iiwalter <iwalterhcf@outlook.com>
2019-12-05 17:38:19 -0300

    Adicionar as forms com validações de campos, requisição POST e retorno da requisição

--- website/views.py
+++ website/views.py
@@ -1,6 +1,37 @@
 from django.shortcuts import render
-
+from website.forms import CadastroForm
+from website.forms import AlagouForm
+from website.models import *
 # Create your views here.
 
-def home(request):
+def layout(request):
     return render(request, 'layout.html')
+
+def home(request):
+    return render(request, 'home.html')
+
+def alagou(request):
+    form = AlagouForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        context = {
+            'msg': "Alagamento cadastrado com sucesso"
+        }
+        return render(request, 'alagou.html', context)
+    context = {
+        'formulario':form
+    }
+    return render(request, 'alagou.html', context)
+
+def cadastro(request):
+    form = CadastroForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        context = {
+            'msg': "Cadastro efetuado com sucesso"
+        }
+        return render(request, 'cadastro.html', context)
+    context = {
+        'formulario':form
+    }
+    return render(request, 'cadastro.html', context)
