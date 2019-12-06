def5ae0c234a2156bb18fe2d5d95a98362675d9c
iiwalter <iwalterhcf@outlook.com>
2019-12-06 20:33:37 -0300

    Adicionar configuração em js para renderizar um mapa na nova página

---
+++ website/static/script.js
@@ -0,0 +1,7 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiaWl3YWx0ZXIiLCJhIjoiY2szdW94ZWJ6MGZyNDNqcHdkaDkyZGdwOCJ9.IZ6e5AMNOVvdf-zbNrKdug';
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11'
+});
+
+var nav = responsiveNav("#nav-collapse");
