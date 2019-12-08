62b93fca29f8b5854c809a00e368c34677c15367
iiwalter <iwalterhcf@outlook.com>
2019-12-07 23:28:10 -0300

    Adicionar nova função para criar usuario e logar usuario

--- website/views.py
+++ website/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from website.forms import CadastroForm
 from website.forms import AlagouForm
 from website.models import *
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def layout(request):
@@ -11,6 +13,7 @@ def layout(request):
 def home(request):
     return render(request, 'home.html')
 
+@login_required
 def alagou(request):
     form = AlagouForm(request.POST or None)
     if form.is_valid():
@@ -21,17 +24,18 @@ def alagou(request):
         return render(request, 'alagou.html', context)
     context = {
         'formulario':form
-    }
+    }  
     return render(request, 'alagou.html', context)
 
 def cadastro(request):
     form = CadastroForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-        context = {
-            'msg': "Cadastro efetuado com sucesso"
-        }
-        return render(request, 'cadastro.html', context)
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            # context = {
+            #     'msg': "Cadastro efetuado com sucesso"
+            # }
+        return render(request, 'cadastro.html')
     context = {
         'formulario':form
     }
@@ -40,3 +44,11 @@ def cadastro(request):
 def local(request):
     locals = Alagou.objects.all().order_by('bairro')
     return render(request, 'locaisalagados.html', {'locals': locals})
+
+def login_user(request):
+    if request.method == 'POST':
+        user = authenticate(username=request.POST['username'], password=request.POST['password'])
+        if user is not None:
+            login(request, user)
+            return redirect('')
+    return render(request, 'login.html')
