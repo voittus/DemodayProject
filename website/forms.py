31039e46bc7a69bfa6f1f87793e68c5be65a0c5e
iiwalter <iwalterhcf@outlook.com>
2019-12-07 23:00:08 -0300

    Atualizar form de cadastro e criptografar senha

--- website/forms.py
+++ website/forms.py
@@ -1,21 +1,38 @@
 from django import forms
 from website.models import Cadastro
 from website.models import Alagou
+from django.contrib.auth.models import User
+
+
+
 class CadastroForm(forms.ModelForm):
+    User._meta.get_field('email').blank = False
+    User._meta.get_field('password').blank = False
     class Meta:
-        model = Cadastro
+        model = User
         fields = [
-            
-            'nome',
+            'username',
             'email',
-            'senha',
+            'password',
         ]
+        widgets = {
+            'username':forms.TextInput(attrs={'class': 'form-control', 'maxlength':140 }),
+            'password':forms.PasswordInput(attrs={'class': 'form-control', 'maxlength':255 })
+        }
+    def save(self, commit=True):
+        user = super(CadastroForm, self).save(commit = False)
+        user.set_password(self.cleaned_data['password'])
+        if commit:
+            user.save()
+        return user
+
 
 class AlagouForm(forms.ModelForm):
     class Meta:
         model = Alagou
         fields = [
+            'rua',
             'bairro',
-            'situacao',
             'trafego',
         ]
+
--- website/models.py
+++ website/models.py
@@ -1,16 +1,21 @@
 from django.db import models
 from django import forms
+from django.contrib.auth.models import User
 # Create your models here.
 class Cadastro(models.Model):
     nome = models.CharField(max_length=140)
     email = models.EmailField()
-    senha = models.CharField(max_length=140)
+    senha = models.CharField(max_length=255)
+    widgets = {
+        'password':forms.PasswordInput(attrs={'class': 'form-control', 'maxlength':255 })
+
+    }
     def __str__(self):
         return self.nome
-
 class Alagou(models.Model):
+    rua = models.CharField(max_length=300)
     bairro = models.CharField(max_length=200)
-    situacao = models.CharField(max_length=300)
     trafego = models.CharField(max_length=150)
     def __str__(self):
         return self.bairro
+
