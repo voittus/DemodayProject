6fccd26dc0ea30cb09e9adb071fc8261990fdedf
iiwalter <iwalterhcf@outlook.com>
2019-12-08 21:14:58 -0300

    Adicionar nova função no mapa do google

--- website/static/script.js
+++ website/static/script.js
@@ -1,26 +1,28 @@
+var map
+var markers = []
 function initMap() {
-    // The location of Uluru
     var sp = { lat: -23.5489, lng: -46.6388 };
-    // The map, centered at sp
     var map = new google.maps.Map(
         document.getElementById('map'), { zoom: 11, center: sp });
-    // The marker, positioned at sp
-    var markers = new google.maps.Marker(
-        {
-            position: new google.maps.LatLng(-23.5356, -46.5272),
-            map: map
-        },
+        var marker = new google.maps.Marker({position: sp, map: map});
+        // Add a marker clusterer to manage the markers.
+        
+    }
+    initMap();
+// function marcadores() {
+//     $.each(points, function (key, point) {
+//         var position = new google.maps.LatLng(point.lat, point.lng);
+//         var marker = new google.maps.Marker(
+//             {
+//                 position: position,
+//                 map: map
+//             })
 
-    );
-    var markers = new google.maps.Marker({
-        position: new google.maps.LatLng(-23.5271, -46.6593),
-        map: map
-    })
-}
-initMap();
+//             markers.push(marker)
+
+//         });
+// }
 
 
 
-
-var nav = responsiveNav("#nav-collapse");
 
