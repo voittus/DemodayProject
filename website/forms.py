14ae6d3cfc724138aa7c5f499fce15d6ef2c0f92
Walter Henrique Costa Ferreira <walter.ferreira@rcky.com.br>
2019-12-10 21:57:50 -0300

    Adicionar funções para retornar uma pesquisa do banco, conforme o digitado no formulário

--- website/forms.py
+++ website/forms.py
@@ -4,11 +4,12 @@ from website.models import Alagou
 from django.contrib.auth.models import User
 
 
-
 class CadastroForm(forms.ModelForm):
     User._meta.get_field('email').blank = False
     User._meta.get_field('password').blank = False
-    password = forms.CharField(max_length=16, widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Senha'}))
+    password = forms.CharField(max_length=16, widget=forms.PasswordInput(
+        attrs={'class': 'form-control', 'placeholder': 'Senha'}))
+
     class Meta:
         model = User
         fields = [
@@ -17,15 +18,17 @@ class CadastroForm(forms.ModelForm):
             'password',
         ]
         widgets = {
-            'username':forms.TextInput(attrs={'class': 'form-control', 'maxlength':140, 'placeholder': 'Username' }),
-            'email':forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email'})
+            'username': forms.TextInput(attrs={'class': 'form-control', 'maxlength': 140, 'placeholder': 'Username'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email'})
         }
+
     def save(self, commit=True):
-        user = super(CadastroForm, self).save(commit = False)
+        user = super(CadastroForm, self).save(commit=False)
         user.set_password(self.cleaned_data['password'])
         if commit:
             user.save()
         return user
+
 
 class AlagouForm(forms.ModelForm):
     class Meta:
@@ -36,11 +39,19 @@ class AlagouForm(forms.ModelForm):
             'trafego',
         ]
         widgets = {
-            'rua':forms.TextInput(attrs={'class': 'form-control', 'maxlength':140, 'placeholder': 'Endereço alagado' }),
-            'bairro':forms.TextInput(attrs={'class': 'form-control', 'maxlength':140, 'placeholder': 'Bairro' }),
-            'trafego':forms.TextInput(attrs={'class': 'form-control', 'maxlength':140, 'placeholder': 'transito' })
+            'rua': forms.TextInput(attrs={'class': 'form-control', 'maxlength': 140, 'placeholder': 'Endereço alagado'}),
+            'bairro': forms.TextInput(attrs={'class': 'form-control', 'maxlength': 140, 'placeholder': 'Bairro'}),
+            'trafego': forms.TextInput(attrs={'class': 'form-control', 'maxlength': 140, 'placeholder': 'transito'})
         }
 
+
 class LoginForm(forms.Form):
-    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'UserName'}))
-    password = forms.CharField(max_length=16, widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Senha'}))
+    username = forms.CharField(widget=forms.TextInput(
+        attrs={'class': 'form-control', 'placeholder': 'UserName'}))
+    password = forms.CharField(max_length=16, widget=forms.PasswordInput(
+        attrs={'class': 'form-control', 'placeholder': 'Senha'}))
+
+
+class PesquisaForm(forms.Form):
+    pesquisa = forms.CharField(widget=forms.TextInput(
+        attrs={'class': 'form-control', 'placeholder': 'Pesquisar rua'}))
